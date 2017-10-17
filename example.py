0be0dcbaa1e2e3cc4d4b1e028dbbce581f1dda36
shadesfear <chris@thecarmans.com>
2017-10-17 13:45:00 +0200

    he

--- example.py
+++ example.py
@@ -31,7 +31,7 @@ hx.set_reference_unit(92)
 hx.reset()
 hx.tare()
 
-file = open("values.txt","rw")
+file = open("values.txt","w")
 
 
 while True:
@@ -45,8 +45,8 @@ while True:
         
         # Prints the weight. Comment if you're debbuging the MSB and LSB issue.
 
-        file.write(val = hx.get_weight(30))
-        print val
+        file.write(hx.get_weight(30))
+        #print val
 
         hx.power_down()
         hx.power_up()
